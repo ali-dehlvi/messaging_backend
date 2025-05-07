02785ba20956dcb3af7901c0ae020b48c963cba6
unknown <alidehlvi082@gmail.com>
2025-05-07 19:46:50 +0530

    fix condition for search user API

--- custom_services/social_actions/__init__.py
+++ custom_services/social_actions/__init__.py
@@ -2,12 +2,14 @@ from fastapi import APIRouter
 from .schemas import SearchUsersResponse, UserOut
 from utils.dependencies import user_verify_dependency,psql_dependency
 from utils.psql.models import User
+from sqlalchemy import or_
 
 social_actions_router = APIRouter(prefix="/social_actions", tags=["SocialActions"])
 
 @social_actions_router.get("/search_users", response_model=SearchUsersResponse)
-async def search_users(q:str, limit=100, offset=0, user_data=user_verify_dependency, psql_db=psql_dependency):
-    users = psql_db.query(User).where(User.email.contains(q) or User.display_name.contains(q)).order_by(User.email.asc()).all()
+async def search_users(q:str, limit: int=100, offset: int=0, user_data=user_verify_dependency, psql_db=psql_dependency):
+    
+    users = psql_db.query(User).where(or_(User.email.contains(q), User.display_name.contains(q))).order_by(User.email.asc()).all()
     start = offset
     end = offset+limit
     return SearchUsersResponse(
