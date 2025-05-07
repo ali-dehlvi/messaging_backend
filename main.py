22106ee26bd0a5469188542bb41e279f72c139e9
unknown <alidehlvi082@gmail.com>
2025-05-07 17:30:24 +0530

    cors middleware fix

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost", "https://messaging-backend-6rbg.onrender.com"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
